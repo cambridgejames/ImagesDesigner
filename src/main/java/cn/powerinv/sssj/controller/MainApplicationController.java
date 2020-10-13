b1dc5a29a256b37043956644e697fba7ec3b77ae
powerinv <cambridge_james@foxmail.com>
2020-10-14 00:36:59 +0800

    修改并完善菜单栏和工具栏按钮的背景颜色样式

--- src/main/java/cn/powerinv/sssj/controller/MainApplicationController.java
+++ src/main/java/cn/powerinv/sssj/controller/MainApplicationController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -16,10 +17,16 @@ public class MainApplicationController {
 
     @FXML public HBox filePathPanel;
     @FXML public BorderPane mainPanel;
+
     @FXML public VBox projectButtonPanel;
+    private ToggleGroup projectButtonGroup;
+    @FXML public ToggleButton projectTreeButton;
+
     @FXML public VBox pluginButtonPanel;
+    private ToggleGroup pluginButtonGroup;
     @FXML public ToggleButton dataSourceButton;
     @FXML public ToggleButton layerButton;
+
     @FXML public VBox bottomPanel;
     @FXML public HBox consoleButtonPanel;
     @FXML public HBox statePanel;
@@ -30,6 +37,13 @@ public class MainApplicationController {
 
     @FXML
     public void initialize() {
+        this.projectButtonGroup = new ToggleGroup();
+        projectTreeButton.setToggleGroup(pluginButtonGroup);
+
+        this.pluginButtonGroup = new ToggleGroup();
+        dataSourceButton.setToggleGroup(pluginButtonGroup);
+        layerButton.setToggleGroup(pluginButtonGroup);
+
         leftStatusBar.setText("Card designer is ready (a minute ago)");
     }
 }
